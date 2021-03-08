e9b58bd89d35040ed435695f5c5264629af79e90
Luke Sprangers <sprangersluke@gmail.com>
2021-03-07 21:13:08 -0600

    change up structure

--- arm1.py
+++ arm1.py
@@ -1,1 +1,9 @@
-some offshoot script
+
+from iacorpus import load_dataset
+
+dataset = load_dataset('fourforums')
+print(dataset.dataset_metadata)
+for discussion in dataset:
+    print(discussion)
+    for post in discussion:
+        print(post)
